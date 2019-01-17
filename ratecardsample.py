d77eac9c57444f7cc88b92eebe8f134f7f2cf9ea
Christopher Avila Holguin <christopher@rstor.io>
2019-01-17 13:03:49 +0100

    parse url and get offers IDs

--- ratecardsample.py
+++ ratecardsample.py
@@ -1,6 +1,10 @@
-from azure.mgmt.commerce import UsageManagementClient
 from azure.common.credentials import ServicePrincipalCredentials
+from azure.mgmt.commerce import UsageManagementClient
+from azure.mgmt.resource import SubscriptionClient
 import os
+import urllib2
+from bs4 import BeautifulSoup
+import json
 
 class PriceManager(object):
     def __init__(self):
@@ -17,15 +21,42 @@ class PriceManager(object):
             self.subscription_id
         )
 
-    def getOfferDurableId(self):
+        self.offersOutputFile = "offersCodes.json"
 
+    def currentSubscriptionName(self):
+        subscriptionClient = SubscriptionClient(self.credentials)
+        for subscription in subscriptionClient.subscriptions.list():
+            if subscription.state == "Enabled":
+                return subscription.display_name
+
+    def getListOfOffers(self):
+        quote_page = 'https://azure.microsoft.com/es-es/support/legal/offer-details/'
+        page = urllib2.urlopen(quote_page)
+        soup = BeautifulSoup(page, 'html.parser')
+        section = soup.find('section', attrs={'aria-label': 'Ofertas actuales'})
+        data = {}
+        for tr in section.find_all('tr')[2:]:
+            tds = tr.find_all('td')
+            data[tds[0].text] = tds[1].text
+        with open(self.offersOutputFile, 'w') as outfile:
+            json.dump(data, outfile)
+
+    def getOfferDurableId(self, offerType):
+        with open(self.offersOutputFile, 'r') as fp:
+            offers = json.load(fp)
+            return offers[offerType]
 
     def getRateObject(self):
+
+        offerId = self.getOfferDurableId(self.currentSubscriptionName())
+
         return self.commerce_client.rate_card.get(
-            "OfferDurableId eq 'MS-AZR-0003P' and Currency eq 'USD' and Locale eq 'en-US' and RegionInfo eq 'US'"
+            "OfferDurableId eq 'MS-AZR-{offerId}' and Currency eq 'USD' and Locale eq 'en-US' and RegionInfo eq 'US'".format(offerId = offerId)
         )
 
 price_manager = PriceManager()
+price_manager.getListOfOffers()
+
 # https://docs.microsoft.com/es-es/python/api/azure-mgmt-commerce/azure.mgmt.commerce.models.resourceratecardinfo?view=azure-python
 rate = price_manager.getRateObject()
 
